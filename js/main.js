79fa42c92d584b464eca00b2d840d6ada499f592
Jonatan Östling <jonatan.ostling@gmail.com>
2015-12-03 16:51:01 +0100

    added my own js file
    toggle function on 2 places

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#button1").click(function(){
+        $(".albumFactsDiv").toggle(700);
+    });
+});
+
+$(document).ready(function(){
+    $("#button2").click(function(){
+        $(".spotifyInfoDiv").toggle(700);
+    });
+});
